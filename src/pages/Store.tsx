c97aa8a189344eb47ec55699f02c73376358bc6a
THREEhundo <samuelbaik87@gmail.com>
2023-05-22 18:52:49 -0700

    added items to store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,20 @@
+import { Col, Row } from 'react-bootstrap'
+import storeItems from '../data/items.json'
+import StoreItem from '../components/StoreItem'
 
 const Store = () => {
-  return (
-	<div>Store</div>
-  )
+	return (
+		<>
+			<h1>Store</h1>
+			<Row>
+				{storeItems.map(item => (
+					<Col md={2} lg={3} xs={1}>
+						<StoreItem {...item} />
+					</Col>
+				))}
+			</Row>
+		</>
+	)
 }
 
 export default Store
