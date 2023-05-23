a8e57d76e8536fc44a0ec99383acfcd53897a91d
THREEhundo <samuelbaik87@gmail.com>
2023-05-22 21:02:13 -0700

    context logic added

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -11,7 +11,10 @@ type ShoppingCartContext = {
 	removeFromCart: (id: number) => void
 }
 
-type CartItem = { id: number; quantity: number }
+type CartItem = {
+	id: number
+	quantity: number
+}
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext)
 
@@ -20,9 +23,56 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
-	const [cartItems, setCartItems] = useState<CartItem[]>
+	const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+	function getItemQuantity(id: number) {
+		return cartItems.find(item => item.id === id)?.quantity || 0
+	}
+
+	function increaseCartQuantity(id: number) {
+		setCartItems(currItems => {
+			if (currItems.find(item => item.id === id) == null) {
+				return [...currItems, { id, quantity: 1 }]
+			} else {
+				return currItems.map(item => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity + 1 }
+					} else {
+						return item
+					}
+				})
+			}
+		})
+	}
+
+	function decreaseCartQuantity(id: number) {
+		setCartItems(currItems => {
+			if (currItems.find(item => item.id === id)?.quantity === 1) {
+				return currItems.filter(item => item.id !== id)
+			} else {
+				return currItems.map(item => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity - 1 }
+					} else {
+						return item
+					}
+				})
+			}
+		})
+	}
+
+	function removeFromCart(id: number) {
+		setCartItems(currItems => currItems.filter(item => item.id !== id))
+	}
+
 	return (
-		<ShoppingCartContext.Provider value={{}}>
+		<ShoppingCartContext.Provider
+			value={{
+				getItemQuantity,
+				increaseCartQuantity,
+				decreaseCartQuantity,
+				removeFromCart
+			}}>
 			{children}
 		</ShoppingCartContext.Provider>
 	)
